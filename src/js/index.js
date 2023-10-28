3edd3fa0e9c4c55906cffbd4a04cd09d7b4cb18d
Aaron <aaron.oconnell@gmail.com>
2023-10-27 21:54:21 -0400

    added header

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 import '../styles/main.css';
 
+const header = document.createElement('header');
+header.setAttribute('class', 'header');
+const logo = document.createElement('span');
+logo.setAttribute('class', 'logo');
+logo.innerText = "RWAlbums";
+header.appendChild(logo);
+
 const root = document.createElement('div');
 root.setAttribute('id', 'root');
-root.innerHTML = 'RWAlbums FTW';
+root.innerHTML = 'main content here';
 
+document.body.appendChild(header);
 document.body.appendChild(root);
